6d7e13ddf4a7360a2b0a4e376eb6e5074316ce9c
Ivan Sosnovich <isosnovich@yandex.ru>
2023-11-26 14:58:25 +0300

    initial state

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+
+import AppLayout from "hostApp/AppLayout";
+
+import App from "./app";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+        <AppLayout>
+    <App />
+    </AppLayout>
+  </React.StrictMode>,
+);
